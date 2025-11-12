3a1b392ba214015529c0dcdcebe63a622458ce45
IT22026170chathuri <135439160+IT22026170chathuri@users.noreply.github.com>
2025-11-12 15:25:14 +0530

    feat: create Cart page with responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
-//import BookDetails from './pages/BookDetails';
+import BookDetails from './pages/BookDetails';
 import Cart from './pages/Cart';
 import Admin from './pages/Admin';
 
