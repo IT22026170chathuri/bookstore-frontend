8fd292753314436108f8135ca575052d181a749f
IT22026170chathuri <135439160+IT22026170chathuri@users.noreply.github.com>
2025-11-12 16:43:47 +0530

    feat: update Navbar to show user-specific content

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import { UserProvider } from './context/UserContext';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import BookDetails from './pages/BookDetails';
 import Cart from './pages/Cart';
 import Admin from './pages/Admin';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/catalog" element={<Catalog />} />
-            <Route path="/book/:id" element={<BookDetails />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <UserProvider>
+      <CartProvider>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/book/:id" element={<BookDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route 
+                  path="/admin" 
+                  element={
+                    <ProtectedRoute requireAdmin>
+                      <Admin />
+                    </ProtectedRoute>
+                  } 
+                />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </CartProvider>
+    </UserProvider>
   );
 }
 
